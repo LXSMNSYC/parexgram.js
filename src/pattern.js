45902e8d98da00488531539eece6d014900b418d
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-18 11:08:54 +0800

    Update pattern.js

--- src/pattern.js
+++ src/pattern.js
@@ -75,8 +75,7 @@ export default class Pattern extends Matcher {
       if (typeof this.value !== 'undefined') {
         if (this.value.test(r)) {
           const m = this.value.exec(r);
-          if (m !== null && r.startsWith(m[0])) {
-            feed.eat(m[0]);
+          if (m !== null && feed.eat(m[0])) {
             return m[0];
           }
         }
